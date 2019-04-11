33f099af7cbf222e7ce0767663f3d55eb33b5519
Tim White <twhite@fool.com>
2019-04-10 21:31:27 -0600

    init

--- src/functions/test.js
+++ src/functions/test.js
@@ -1,6 +1,6 @@
 exports.handler = (event, context, callback) => {
     callback(null, {
         statusCode: 200,
-        body: 'Lammy Lammy Gumdrop!'
+        body: 'Lammy Lammy Gumdrops!'
     })
 };
